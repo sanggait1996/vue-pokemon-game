474945de949aacd8c29e92a2acb8ff941a7840b8
Sang Nguyễn <sanggait1996@gmail.com>
2021-12-28 15:53:37 +0700

    create game and push to git

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)"></main-screen>
+    <interact-screen 
+    v-if="statusMatch === 'ingame'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+    />
+    <result-screen 
+    v-if="statusMatch === 'finished'" 
+    :timer="timer" 
+    @onStartAgain="statusMatch = 'default'"
+    />
+    <copy-right />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainScreen from "./components/MainScreen.vue"
+import InteractScreen from "./components/InteractScreen.vue"
+import ResultScreen from "./components/ResultScreen.vue";
+import CopyRight from "./components/CopyRight.vue";
+
+import { shuffled } from "./ultils/array"
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+    name: "App",
+    components: {
+        MainScreen,
+        InteractScreen,
+        ResultScreen,
+        CopyRight,
+    },
+    data() {
+        return {
+            settings: {
+                totalBlocks: 0,
+                cardsContext: [],
+                startedAt: null,
+            },
+            statusMatch: "default",
+            timer: 0,
+        }
+    },
+    methods: {
+        onHandleBeforeStart(config) {
+            this.settings.totalBlocks = config.totalBlocks
+            const firstCards = Array.from({ length: this.settings.totalBlocks / 2}, 
+            (_, i) => i + 1
+            )
+            const secondCards = [...firstCards]
+            const cards = [...firstCards, ...secondCards]
+            this.settings.cardsContext = shuffled(shuffled(shuffled(shuffled(cards))))
+            this.settings.startedAt = new Date().getTime();
+
+            // data ready
+
+            this.statusMatch = "ingame"
+        },
+        onGetResult() {
+            // get timer
+            this.timer = new Date().getTime() - this.settings.startedAt
+            //switch to result 
+            this.statusMatch = "finished"
+        },
+    }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CopyRight.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="copyright">
+        <p>This game owned by Richard Nguyen - using VueJs 3 - 
+            <a href="https://www.facebook.com/sanggait/" target="_blank">View here</a>
+        </p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.copyright {
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 1.5rem;
+    color: var(--light);
+    z-index: 3;
+    font-size: 1.5rem;
+}
+.copyright a{
+    color: #f4dc26
+}
+</style>
